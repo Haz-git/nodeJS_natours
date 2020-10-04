595b84d1a36e5686cfa2377584474421747beab7
Haz-git <zhouharry1@gmail.com>
2020-10-04 07:41:05 -0700

    Added Post Request Handler

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
+//Using middleware
+app.use(express.json());
 
 // app.get('/', (req, res) => {
 //     res
@@ -13,7 +16,38 @@ const port = 3000;
 //     res.send('You can post to this endpoint');
 // })
 
-app.get('/api/v1/tours')
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+//We should always specify version of api, so when we upgrade we can simply change to version 2.
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'Success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    })
+})
+
+app.post('/api/v1/tours', (req, res) => {
+    console.log(req.body);
+    //req.body availiable on request because we used middleware.
+    const newId = tours[tours.length -1].id + 1;
+    const newTour = Object.assign({
+        id: newId
+    }, req.body);
+
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'Success',
+            data: {
+                tour: newTour
+            }
+        })
+    })
+})
 
 app.listen(port, () => {
     console.log(`App is now running on port ${port}`);
