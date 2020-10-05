3382f190554a3afe912dc05e2b3ef64bad71f7af
Haz-git <zhouharry1@gmail.com>
2020-10-05 05:56:18 -0700

    Separating Express W/ server

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 const express = require('express');
-const fs = require('fs');
-const { pathToFileURL } = require('url');
 const app = express();
-const port = 3000;
 const morgan = require('morgan');
 
 const tourRouter = require('./routes/tourRoutes');
@@ -17,13 +14,6 @@ app.use((req, res, next) => {
     next();
 })
 
-//Reading files
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
-
-//ALL ROUTE HANDLERS
-
-
-
 //We should always specify version of api, so when we upgrade we can simply change to version 2.
 //App Routes
 // app.get('/api/v1/tours', getAllTours);
@@ -35,6 +25,4 @@ const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simpl
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-app.listen(port, () => {
-    console.log(`App is now running on port ${port}`);
-});
+module.exports = app;
