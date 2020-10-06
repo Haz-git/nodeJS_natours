15ab05882b945cbcc10a4de26b2e2e44dc2dfebe
Haz-git <zhouharry1@gmail.com>
2020-10-06 16:29:24 -0700

    Added More Fields to Tour Model

--- models/tourModel.js
+++ models/tourModel.js
@@ -7,15 +7,52 @@ const tourSchema = new mongoose.Schema({
 		required: [true, 'A Tour must have a name!'],
 		unique: true,
 	},
-	rating: {
+	duration: {
+		type: Number,
+		required: [true, 'A Tour must have a duration!']
+	},
+	maxGroupSize: {
+		type: Number,
+		required: [true, 'A Tour must have a group size!']
+	},
+	difficulty: {
+		type: String,
+		required: [true, 'A Tour must have a difficulty']
+	},
+	ratingsAverage: {
 		type: Number,
 		default: 3.0,
+	},
+	ratingsQuantity: {
+		type: Number,
+		default: 0,
 	},
 	price: {
 		type: Number,
 		required: [true, 'A Tour must have a price!'],
 	},
+	priceDiscount: Number,
+	summary: {
+		type: String,
+		trim: true,
+		required: [true, 'A Tour must have a summary'],
+	},
+	description: {
+		type: String,
+		trim: true,
+	},
+	imageCover: {
+		type: String,
+		required: [true, 'A tour must have a cover image'],
+	},
+	images: [String],
+	createdAt: {
+		type: Date,
+		default: Date.now(),
+	},
+	startDate: [Date],
 });
+//Trim cuts all the whitespace.
 
 //Creating Model from Schema
 const Tour = mongoose.model('Tour', tourSchema);
