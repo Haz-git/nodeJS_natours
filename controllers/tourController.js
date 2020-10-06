2e5d002a28546b5b540ecd92acc3f4498f865fa7
Haz-git <zhouharry1@gmail.com>
2020-10-06 12:43:41 -0700

    Refactored getTour and getAllTours Controllers

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,62 +5,75 @@ const Tour = require('../models/tourModel');
 // 	fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 // );
 
-exports.getAllTours = (req, res) => {
-	console.log(req.requestTime);
+exports.getAllTours = async (req, res) => {
+    try {
+        const tours = await Tour.find();
 
-	res.status(200).json({
-		status: 'Success',
-		requestedAt: req.requestTime,
-		// results: tours.length,
-		// data: {
-		// 	tours,
-		// },
-	});
+        res.status(200).json({
+            status: 'Success',
+            results: tours.length,
+            data: {
+                tours,
+            },
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err,
+        });
+    }
 };
 
-exports.getTour = (req, res) => {
-	const id = req.params.id * 1;
-	// const tour = tours.find((el) => el.id === id);
+exports.getTour = async (req, res) => {
+    try {
+        const tour = await Tour.findById(req.params.id);
+        //findById is a helper function for finding Mongo's '_id'.
 
-	// res.status(200).json({
-	// 	status: 'Success',
-	// 	data: {
-	// 		tour,
-	// 	},
-	// });
+        res.status(200).json({
+            status: 'Success',
+            data: {
+                tour,
+            },
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'Failed',
+            message: err,
+        });
+    }
 };
 
 exports.createTour = async (req, res) => {
-	try {
-		const newTour = await Tour.create(req.body);
+    try {
+        const newTour = await Tour.create(req.body);
 
-		res.status(201).json({
-			status: 'Success',
-			data: {
-				tour: newTour,
-			},
-		});
-	} catch (err) {
-		res.status(400).json({
-			status: 'Fail',
-			message: err,
-		});
-	}
+        res.status(201).json({
+            status: 'Success',
+            data: {
+                tour: newTour,
+            },
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'Fail',
+            message: err,
+        });
+    }
 };
 
 exports.updateTour = (req, res) => {
-	res.status(200).json({
-		status: 'Success',
-		data: {
-			tour: 'Updated Tour here...',
-		},
-	});
+    res.status(200).json({
+        status: 'Success',
+        data: {
+            tour: 'Updated Tour here...',
+        },
+    });
 };
 
 exports.deleteTour = (req, res) => {
-	//Status code 204 for 'no content'
-	res.status(204).json({
-		status: 'Success',
-		data: null, //This data is null cause no new information on delete.
-	});
+    //Status code 204 for 'no content'
+    res.status(204).json({
+        status: 'Success',
+        data: null, //This data is null cause no new information on delete.
+    });
 };
