0a38a5f367e71e42331aaa540e3ecc259817bb2e
Haz-git <zhouharry1@gmail.com>
2020-10-09 09:02:28 -0700

    Adding Global Error Handling Middleware

--- app.js
+++ app.js
@@ -31,10 +31,30 @@ app.use('/api/v1/users', userRouter);
 
 //Handle all uncaught routes:
 app.all('*', (req, res, next) => {
-	res.status(404).json({
-		status: 'Fail',
-		message: `Can't find ${req.originalUrl} on this server. Are you sure this is the correct URL?`,
-	});
+	// res.status(404).json({
+	// 	status: 'Fail',
+	// 	message: `Can't find ${req.originalUrl} on this server. Are you sure this is the correct URL?`,
+	// });
+	const err = new Error(`Can't find ${req.originalUrl} on this server. Are you sure this is the correct URL?`)
+	err.status = 'fail';
+	err.statusCode = 404;
+
+	//When you pass ANYTHING into next, Express will automatically assume that it is an error. Then, it will ignore all of the middleware in our middleware stack and send the error to our global middleware handler.
+	next(err);
 });
+
+//Creating global error handling middleware:
+
+app.use((err, req, res, next) => {
+	//When you specify these four parameters, Express automatically knows that it's an error handler and only calls this when there's an error.
+	err.statusCode = err.statusCode || 500;
+	//If statusCode is defined, use that code, else use 500 (internal server error)
+	err.status = err.status || 'error';
+	//If status is defined, use it. Else, use 'error'.
+	res.status(err.statusCode).json({
+		status: err.status,
+		message: err.message
+	})
+})
 
 module.exports = app;
