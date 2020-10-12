2f0e13764271f7231eaa8fbf4f47d173a7a8fd31
Haz-git <zhouharry1@gmail.com>
2020-10-12 12:37:29 -0700

    Added User Sign In with JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,24 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm,
+    });
+
+    const token = jwt.sign({ id: newUser.__id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    });
 
     res.status(201).json({
         status: 'Success!',
+        token,
         data: {
-            user: newUser
-        }
+            user: newUser,
+        },
     });
 });
